7ff15532a4273083d402faa6361199c506a12959
HusamIzzat <79759988+HusamIzzat@users.noreply.github.com>
2023-02-26 13:46:38 +0200

    remove modal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import DoctorCard from "./DoctorCard/DoctorCard";
 import DoctorCardDetails from "./DoctorCard/DoctorCardDetails";
 import Header from "./Header/Header";
 import Breadcrumbs from "./UI/Breadcrumbs";
-import Modal from "./UI/Modal";
 
 const items = [
   {
